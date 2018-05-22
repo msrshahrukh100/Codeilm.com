4b2e4e6d888d131ce44ae56f72efe3b7a6e1fde9
Shahrukh <msr.concordfly@gmail.com>
2018-05-22 23:00:04 +0530

    Added logger to track errors

--- feedback/views.py
+++ feedback/views.py
@@ -3,13 +3,19 @@ from .models import FeedbackEvent
 from django.shortcuts import get_object_or_404
 from django.template import Template, Context
 from . import utils
+import logging
 # Create your views here.
+
+logger = logging.getLogger(__name__)
 
 
 def feedback_page(request, id=None, slug=None):
 	feedback_event = get_object_or_404(FeedbackEvent, id=id, slug=slug)
+	logger.info("Feedback page request reached")
 	if request.method == "POST":
+		logger.info("POST request in the feedback_page view")
 		utils.save_feedback_response(request, feedback_event)
+		logger.info("Feedback response saved")
 		return render(request, "feedback_thankyou.html", {})
 	template = Template(feedback_event.click_response_html)
 	context_for_click_response_html = Context({"feedback_event": feedback_event})
