79efd19dbcf7e0191f9ba35df3ebcdd795cf9138
Shahrukh <msr.concordfly@gmail.com>
2018-05-23 20:03:05 +0530

    Updated the urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import include, path
-from .views import home, mark_notifications_read
+from django.urls import path
+from .views import home, mark_notifications_read, redirect_to_page
 
 urlpatterns = [
     path('', home, name="home"),
+    path('redirect/', redirect_to_page, name="redirect_to_page"),
     # path('<slug:community>/', community_mapper, name="community_mapper")
     path('mark-notifications-read/', mark_notifications_read, name="mark_notifications_read"),
 ]
