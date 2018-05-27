11f01579d4cd71e732b633f53a1463db87b66b15
Shahrukh <msr.concordfly@gmail.com>
2018-05-27 15:53:45 +0530

    Debugging

--- Bismillah/settings.py
+++ Bismillah/settings.py
@@ -84,7 +84,7 @@ AUTHENTICATION_BACKENDS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "templates", "allauth")],
+        'DIRS': [],  # os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "templates", "allauth")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
