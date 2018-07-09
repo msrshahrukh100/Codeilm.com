98e605b5e0bee2cbff05a7dddfdb165268e1beb7
Shahrukh <msr.concordfly@gmail.com>
2018-07-09 08:03:28 +0530

    Updated the view buddy

--- mainapp/templatetags/main_extras.py
+++ mainapp/templatetags/main_extras.py
@@ -7,4 +7,9 @@ register = template.Library()
 
 @register.filter(name='static_absolute_uri')
 def static_absolute_uri(value):
-    return settings.BASE_URL + static(value)
+	return settings.BASE_URL + static(value)
+
+
+@register.filter(name='first_n_items')
+def first_n_items(value, n):
+	return value[:int(n)]
