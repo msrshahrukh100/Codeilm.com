46966300432fe7b53d3b163268667bb43589ced4
Shahrukh <msr.concordfly@gmail.com>
2018-08-06 21:52:36 +0530

    Updated error

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, mark_notifications_read, redirect_to_page, group_join, save_group_creation_request, how_it_works
+from .views import home, mark_notifications_read, redirect_to_page, group_join, save_group_creation_request, how_it_works, redirect_for_server_error
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('how-it-works/', how_it_works, name="how_it_works"),
     path('redirect/', redirect_to_page, name="redirect_to_page"),
+    path('will-be-back/', redirect_for_server_error, name="redirect_for_server_error"),
     path('save-group-creation-request/', save_group_creation_request, name="save_group_creation_request"),
     # path('<slug:community>/', community_mapper, name="community_mapper")
     path('<str:community>/<int:id>/<slug:slug>/join', group_join, name="group_join"),
