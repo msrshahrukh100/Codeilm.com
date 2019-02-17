3e533ce7d2a0b4fda9d436ac566affa6e6417123
Shahrukh <msr.concordfly@gmail.com>
2019-02-17 10:27:59 +0530

    Fixed the urls

--- androidapps/gymapp/gymappbackend/urls.py
+++ androidapps/gymapp/gymappbackend/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views as gym_views
 
 urlpatterns = [
-    path('gym-home/', gym_views.home, name="gym_home"),
+    re_path('gym-home/', gym_views.home, name="gym_home"),
+    # re_path(r'^gym-home/$', gym_views.home, name="gym_home"),
+    # re_path(r'^gym-home/(?P<slug>[\w-]+)$', gym_views.home, name="gym_home"),
 ]
