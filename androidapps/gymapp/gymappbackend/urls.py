79256cbc076142e39e811f8a7703101d184b0eed
Shahrukh <msr.concordfly@gmail.com>
2019-02-17 11:30:25 +0530

    Added login page for the gym app base

--- androidapps/gymapp/gymappbackend/urls.py
+++ androidapps/gymapp/gymappbackend/urls.py
@@ -3,8 +3,8 @@ from . import views as gym_views
 
 urlpatterns = [
     re_path('gym-home/', gym_views.home, name="gym_home"),
-
     path('api/exercises/', gym_views.ExerciseList.as_view()),
+    path('login/', gym_views.login, name="gymloginpage"),
     # re_path(r'^gym-home/$', gym_views.home, name="gym_home"),
     # re_path(r'^gym-home/(?P<slug>[\w-]+)$', gym_views.home, name="gym_home"),
 ]
--- androidapps/gymapp/gymappbackend/views.py
+++ androidapps/gymapp/gymappbackend/views.py
@@ -4,13 +4,15 @@ from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from . import serializers as gymapp_serializers
 from . import models as gymapp_models
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='/android/login')
 def home(request):
 	return render(request, "gym_index.html", {})
 
-
+def login(request):
+	return render(request, "gymlogin.html", {})
 
 class ExerciseList(generics.ListCreateAPIView):
     queryset = gymapp_models.Exercise.objects.all()
