02b79c9e9fb05fa4464bba4919f8627f60c0822d
Shahrukh <mohammad.shahrukh@go-mmt.com>
2019-04-02 18:42:20 +0530

    Now resolved error

--- lovecode/lovecodebackend/serializers.py
+++ lovecode/lovecodebackend/serializers.py
@@ -5,16 +5,12 @@ class GithubRepoSerializer(serializers.ModelSerializer):
 	hash_id = serializers.CharField(default="")
 	class Meta:
 		model = lovecode_model.GithubRepo
-		fields = ('hash_id', 'user', 'repo_data', 'languages', 'created_at', 'updated_at')
+		fields = ('hash_id', 'user', 'repo_data', 'created_at', 'updated_at')
 
 class TutorialSerializer(serializers.ModelSerializer):
 	hash_id = serializers.CharField(default="")
 	# repo = GithubRepoSerializer(read_only=True)
-	languages = serializers.SerializerMethodField()
-
-	def get_languages(self, obj):
-		return obj.get_languages()
 
 	class Meta:
 		model = lovecode_model.Tutorial
-		fields = ('hash_id', 'user', 'title','slug', 'languages', 'read_time', 'created_at', 'updated_at')
+		fields = ('hash_id', 'user', 'title','slug', 'read_time', 'created_at', 'updated_at')
