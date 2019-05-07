856bd0987f6faa17d3e833bc3135c378299ed567
Shahrukh <mohammad.shahrukh@go-mmt.com>
2019-05-07 14:44:55 +0530

    Added custom renderes for the code block and it works

--- lovecode/lovecodefrontend/lovecode/src/components/TutorialPage/TutorialPage.js
+++ lovecode/lovecodefrontend/lovecode/src/components/TutorialPage/TutorialPage.js
@@ -2,12 +2,24 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import Embed from '../CarbonCode/embed'
 
+const renderers: ReactMarkdown.Renderers = {
+    code: (props: string) => {
+      console.log(props);
+      return (
+        <Embed code={props.value} language="python" />
+      )
+    }
+};
+
 const tutorialPage = (props) => {
   const title = props.page ? props.page.title : null
   const content = props.page ?
     props.page.content.map((segment, index) => {
       if(segment.type == "md") {
-        return <ReactMarkdown key={"markdown" + index} source={segment.value} />
+        return <ReactMarkdown
+          key={"markdown" + index}
+          source={segment.value}
+          renderers={renderers} />
       }
       else if(segment.type == "code") {
         return <Embed key={"code"+index} code={segment.value} language="python" />
