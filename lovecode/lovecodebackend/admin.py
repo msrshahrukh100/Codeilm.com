6c0ef49d448e9ed4572948ec80b11f82ed5ba62c
Shahrukh <msr.concordfly@gmail.com>
2019-05-18 16:15:29 +0530

    Improved admin

--- lovecode/lovecodebackend/admin.py
+++ lovecode/lovecodebackend/admin.py
@@ -12,8 +12,32 @@ class TutorialAdmin(admin.ModelAdmin):
 class GithubApiResponseAdmin(admin.ModelAdmin):
 	list_display = ['user', 'url', 'etag', 'get_params']
 
+
+class TutorialViewAdmin(admin.ModelAdmin):
+	list_display = ['user', 'tutorial', 'city', 'country_name', 'region', 'time_zone']
+
+	def city(self, obj):
+		if obj.request_ip_info:
+			return obj.request_ip_info.city
+		return None
+
+	def country_name(self, obj):
+		if obj.request_ip_info:
+			return obj.request_ip_info.country_name
+		return None
+
+	def region(self, obj):
+		if obj.request_ip_info:
+			return obj.request_ip_info.region
+		return None
+
+	def time_zone(self, obj):
+		if obj.request_ip_info:
+			return obj.request_ip_info.time_zone
+		return None
+
 admin.site.register(lovecode_model.GithubRepo, GithubRepoAdmin)
 admin.site.register(lovecode_model.Tutorial, TutorialAdmin)
 admin.site.register(lovecode_model.GithubApiResponse, GithubApiResponseAdmin)
 admin.site.register(lovecode_model.TutorialLike)
-admin.site.register(lovecode_model.TutorialView)
+admin.site.register(lovecode_model.TutorialView, TutorialViewAdmin)
