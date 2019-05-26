4ccab4a6a9ec958d804fc96b73c988f36a180110
Shahrukh <msr.concordfly@gmail.com>
2019-05-26 10:17:55 +0530

    Added ip

--- lovecode/lovecodefrontend/lovecode/src/lovecodeaxios.js
+++ lovecode/lovecodefrontend/lovecode/src/lovecodeaxios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // baseURL: 'http://localhost:8000/android/gymapp/api/'
 // baseURL: 'https://allywith.com/android/gymapp/api/'
 const instance = axios.create({
-  baseURL: 'https://allywith.com/api/v1'
+  baseURL: 'http://54.218.212.69/api/v1'
 })
 
 export default instance
