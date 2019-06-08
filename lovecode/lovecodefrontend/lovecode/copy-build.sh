9f6293632859bb150bc4c6c7502abd794b0f2ea3
Shahrukh <msr.concordfly@gmail.com>
2019-06-08 14:46:04 +0530

    Added async lode

--- lovecode/lovecodefrontend/lovecode/copy-build.sh
+++ lovecode/lovecodefrontend/lovecode/copy-build.sh
@@ -1,7 +1,10 @@
 npm run build
 
-cat build/static/css/main*.css >  ../../lovecodebackend/static/lovecodebackend/css/main.css
-cat build/static/js/main*.js > ../../lovecodebackend/static/lovecodebackend/js/main.js
-cat build/static/js/*.chunk.js > ../../lovecodebackend/static/lovecodebackend/js/chunk.js
-
-cat build/asset-manifest.json > ../../lovecodebackend/static/lovecodebackend/manifest/asset-manifest.json
+cp -r build/static/* ../../lovecodebackend/static/
+cp -r build/index.html ../../lovecodebackend/templates/lovecode.html
+#
+# cat build/static/css/main*.css >  ../../lovecodebackend/static/lovecodebackend/css/main.css
+# cat build/static/js/main*.js > ../../lovecodebackend/static/lovecodebackend/js/main.js
+# cat build/static/js/*.chunk.js > ../../lovecodebackend/static/lovecodebackend/js/chunk.js
+#
+# cat build/asset-manifest.json > ../../lovecodebackend/static/lovecodebackend/manifest/asset-manifest.json
