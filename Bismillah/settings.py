f25f5d92190b6a6389912d84ed9d2c83b41f47d5
Shahrukh <msr.concordfly@gmail.com>
2019-06-09 11:33:11 +0530

    Changed auth method

--- Bismillah/settings.py
+++ Bismillah/settings.py
@@ -111,9 +111,8 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
 
