6398908ebbc7a67df34db28705499ea3463906ba
Shahrukh <msr.concordfly@gmail.com>
2019-06-22 13:18:09 +0530

    Added ga

--- lovecode/lovecodefrontend/lovecode/src/index.js
+++ lovecode/lovecodefrontend/lovecode/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
 import repoDataReducer from './store/reducers/repoDataReducer'
 import authReducer from './store/reducers/authReducer'
 import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-142472289-1', {debug: false});
+
+const history = createBrowserHistory();
+
+history.listen(function (location) {
+  ReactGA.pageview(location.pathname + location.search)
+})
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -17,17 +27,22 @@ const rootReducer = combineReducers({
   aReducer: authReducer
 })
 
+
+
 const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
 ));
 
+
 const app = (
-  <BrowserRouter basename='/'>
+  <Router basename='/' history={history}>
     <Provider store={store}>
       <App/>
     </Provider>
-  </BrowserRouter>
+  </Router>
 )
+
+
 
 
 ReactDOM.render(app, document.getElementById('root'));
