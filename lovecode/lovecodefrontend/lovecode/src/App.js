5bf83a0e8bab5ca4100d3ea35a1cd6827281cf8a
Shahrukh <msr.concordfly@gmail.com>
2019-06-23 14:30:16 +0530

    Fixed issue

--- lovecode/lovecodefrontend/lovecode/src/App.js
+++ lovecode/lovecodefrontend/lovecode/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import ReactGA from 'react-ga';
 import { connect } from 'react-redux'
-
+import * as actionCreators from './store/actions/index'
 
 class App extends Component {
   componentDidMount() {
     ReactGA.set({ userId: this.props.userId });
+    this.props.auth()
   }
   render() {
     return (
@@ -21,4 +22,10 @@ const matchStateToProps = state => {
   }
 }
 
-export default connect(matchStateToProps, null)(App);
+const matchDispatchToProps = dispatch => {
+  return {
+    auth: () => dispatch(actionCreators.auth())
+  }
+}
+
+export default connect(matchStateToProps, matchDispatchToProps)(App);
