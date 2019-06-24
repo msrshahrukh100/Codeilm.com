5b27bbfebbba114179b573e1e29ac8950cfa946c
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-06-24 16:09:17 +0530

    Completed the endpoint for userprofile

--- usermanagement/serializers.py
+++ usermanagement/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class UserProfilePageSerializer(serializers.ModelSerializer):
-    user_profile_pic = serializers.SerializerMethodField()
-    full_name = serializers.SerializerMethodField()
-    intro = serializers.SerializerMethodField()
+    user_profile_pic = serializers.CharField(read_only=True, source='user_profile.first.get_profile_pic_url')
+    full_name = serializers.CharField(read_only=True, source='get_full_name')
+    intro = serializers.CharField(source='user_profile.first.intro')
 
-    def get_intro(self, obj):
-        return obj.user_profile.first().intro
+    def update(self, instance, validated_data):
+        user_profile = instance.user_profile.first()
+        user_profile.intro = validated_data.get('user_profile', {}).get('first', {}).get('intro', "")
+        user_profile.save()
+        return instance
 
-    def get_full_name(self, obj):
-        return obj.get_full_name()
-
-    def get_user_profile_pic(self, obj):
-        return obj.user_profile.first().get_profile_pic_url()
 
     class Meta:
         model = User
+        read_only_fields = ['id', 'username', 'full_name', 'last_login', 'date_joined']
         fields = ['id', 'username', 'full_name', 'intro', 'user_profile_pic', 'last_login', 'date_joined']
