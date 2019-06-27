ce3799b807fb098a6157816f5a48c380753c5e1b
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-06-27 19:38:11 +0530

    Added connection to serializers

--- usermanagement/serializers.py
+++ usermanagement/serializers.py
@@ -1,15 +1,45 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from . import models as usermanagement_models
 from lovecode.lovecodebackend import serializers as lovecode_serializers
+
+
+class UserSimpleSerializer(serializers.ModelSerializer):
+    user_profile_pic = serializers.CharField(read_only=True, source='user_profile.first.get_profile_pic_url')
+    full_name = serializers.CharField(read_only=True, source='get_full_name')
+    intro = serializers.CharField(source='user_profile.first.intro')
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'full_name', 'intro', 'user_profile_pic']
+
+class FollowingConnectionsSerializer(serializers.ModelSerializer):
+    following = UserSimpleSerializer()
+    class Meta:
+        model = usermanagement_models.Connections
+        fields = ('id', 'user', 'following')
+
+class FollowerConnectionsSerializer(serializers.ModelSerializer):
+    user = UserSimpleSerializer()
+
+    class Meta:
+        model = usermanagement_models.Connections
+        fields = ('id', 'user', 'following')
 
 
 class UserProfilePageSerializer(serializers.ModelSerializer):
     user_profile_pic = serializers.CharField(read_only=True, source='user_profile.first.get_profile_pic_url')
     full_name = serializers.CharField(read_only=True, source='get_full_name')
     intro = serializers.CharField(source='user_profile.first.intro')
-    tutorials = serializers.SerializerMethodField()
     my_profile = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
+    follower = serializers.SerializerMethodField()
 
+    def get_following(self, obj):
+        return FollowingConnectionsSerializer(obj.user_followings.all(), many=True).data
+
+    def get_follower(self, obj):
+        return FollowerConnectionsSerializer(obj.user_followers.all(), many=True).data
 
     def get_my_profile(self, obj):
         user = None
@@ -19,19 +49,6 @@ class UserProfilePageSerializer(serializers.ModelSerializer):
             if request_user.is_authenticated and obj == request_user:
                 return True
         return False
-
-    def get_tutorials(self, obj):
-        is_my_profile = self.get_my_profile(obj)
-        print(is_my_profile)
-        if is_my_profile:
-            print("is_same")
-            tutorials = obj.user_tutorials.all()
-        else:
-            print("is not same")
-            tutorials = obj.user_tutorials.filter(is_published=True)
-
-        
-        return lovecode_serializers.TutorialListSerializer(tutorials, many=True).data
 
     def update(self, instance, validated_data):
         user_profile = instance.user_profile.first()
@@ -43,4 +60,4 @@ class UserProfilePageSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         read_only_fields = ['id', 'username', 'full_name', 'last_login', 'date_joined']
-        fields = ['id', 'username', 'full_name', 'intro', 'my_profile', 'user_profile_pic', 'tutorials', 'last_login', 'date_joined']
+        fields = ['id', 'username', 'full_name', 'intro', 'my_profile', 'user_profile_pic', 'following', 'follower', 'last_login', 'date_joined']
