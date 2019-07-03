a1f95ff341cbb6870e00473b85619e087c081879
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-07-03 15:41:06 +0530

    Changed email

--- emailmanager/utils.py
+++ emailmanager/utils.py
@@ -14,7 +14,7 @@ def send_welcome_email(user):
 	context = {"base_url": settings.BASE_URL, "email": user.email, "template": "emails/welcome_email.html"}
 	context["get_params"] = get_params_from_context(context)
 	emailmanager_tasks.send_ses_email(
-		sender="Welcome to Allywith <shahrukh@allywith.com>",
+		sender="Welcome to Allywith <towardslight52@gmail.com>",
 		user_ids=[user.id],
 		subject="Sent with ❤️ from Allywith",
 		template_path="emails/welcome_email.html",
@@ -25,7 +25,7 @@ def send_welcome_email(user):
 def send_info_mail_to_admins(context):
 	admins = settings.ADMINS
 	emailmanager_tasks.send_ses_email(
-		sender="Admin Info from Allywith <shahrukh@allywith.com>",
+		sender="Admin Info from Allywith <towardslight52@gmail.com>",
 		recipients=admins,
 		subject=context.get('subject'),
 		template_path="emails/send_info_to_admin.html",
