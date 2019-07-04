05d4fcb0d75d982cf71aaf8c2a132ea81761bb30
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-07-04 18:28:44 +0530

    ASF

--- usermanagement/usermanagementfrontend/user/copy-build.sh
+++ usermanagement/usermanagementfrontend/user/copy-build.sh
@@ -2,6 +2,6 @@ npm run build
 
 git rm -r ../../static/js/
 git rm -r ../../static/css/
-#mkdir ../../static/
+mkdir ../../static/
 cp -r build/static/* ../../static/
 cp -r build/index.html ../../templates/usermanagement.html
