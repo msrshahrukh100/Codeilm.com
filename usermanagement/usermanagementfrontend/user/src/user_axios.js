82b5a2fdeb85e5ebc99b324d137c274aeb56d3aa
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-07-12 11:06:24 +0530

    Used relative url

--- usermanagement/usermanagementfrontend/user/src/user_axios.js
+++ usermanagement/usermanagementfrontend/user/src/user_axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // baseURL: 'http://localhost:8000/android/gymapp/api/'
 // baseURL: 'https://allywith.com/android/gymapp/api/'
 const instance = axios.create({
-  baseURL: 'https://codeilm.com/api/usermanagement/'
+  baseURL: '/api/usermanagement/'
 })
 
 export default instance
