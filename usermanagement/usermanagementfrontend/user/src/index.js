61b89fd80a4730f20e8ff3e393a0e74db4b67e25
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-07-13 16:46:57 +0530

    Debug

--- usermanagement/usermanagementfrontend/user/src/index.js
+++ usermanagement/usermanagementfrontend/user/src/index.js
@@ -14,7 +14,7 @@ import ReactGA from 'react-ga';
 
 const debug = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
 
-ReactGA.initialize('UA-142472289-1', {debug: true});
+ReactGA.initialize('UA-142472289-1', {debug: debug});
 
 const history = createBrowserHistory();
 
