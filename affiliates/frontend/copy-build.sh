874981a34f2af5acc6289ca5e296cfab1bbeca55
Shahrukh <msr.concordfly@gmail.com>
2019-08-11 21:35:40 +0530

    temp commit

--- affiliates/frontend/copy-build.sh
+++ affiliates/frontend/copy-build.sh
@@ -2,7 +2,7 @@ npm run build
 
 git rm -r ../static/js/
 git rm -r ../static/css/
-mkdir ../../lovecodebackend/static/
+mkdir ../static/
 cp -r build/static/* ../static/
 cp -r build/index.html ../templates/affiliate.html
 #
