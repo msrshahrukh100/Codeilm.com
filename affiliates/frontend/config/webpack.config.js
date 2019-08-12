3626e92c55cc5294413293f47aaa31727631ee52
Shahrukh <msr.concordfly@gmail.com>
2019-08-12 17:09:04 +0530

    Eorking

--- affiliates/frontend/config/webpack.config.js
+++ affiliates/frontend/config/webpack.config.js
@@ -49,9 +49,13 @@ module.exports = function(webpackEnv) {
   // Webpack uses `publicPath` to determine where the app is being served from.
   // It requires a trailing slash, or the file assets will get an incorrect path.
   // In development, we always serve from the root. This makes config easier.
-  const publicPath = isEnvProduction
-    ? paths.servedPath
-    : isEnvDevelopment && '/';
+
+
+  // const publicPath = isEnvProduction
+  //   ? paths.servedPath
+  //   : isEnvDevelopment && '/';
+
+  const publicPath = "https://codeilm.com/"  
   // Some apps do not use client-side routing with pushState.
   // For these, "homepage" can be set to "." to enable relative asset paths.
   const shouldUseRelativeAssetPaths = publicPath === './';
