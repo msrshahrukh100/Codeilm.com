50f67b0e07ef91ed81713b37ab05bc5ded430aa2
Shahrukh <msr.concordfly@gmail.com>
2019-08-18 13:54:08 +0530

    Workign on projects

--- projects/frontend/projects/src/containers/ProjectsDetail/ProjectsDetail.js
+++ projects/frontend/projects/src/containers/ProjectsDetail/ProjectsDetail.js
@@ -15,17 +15,19 @@ import Chip from '@material-ui/core/Chip';
 import Tooltip from '@material-ui/core/Tooltip';
 import { red } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 
 const styles = theme => ({
   card: {
-    margin: theme.spacing(5),
+    margin: theme.spacing(2),
     [theme.breakpoints.down('sm')]: {
       margin: theme.spacing()
     },
   },
   title: {
       margin: theme.spacing(),
+      marginBottom: theme.spacing(5),
       fontSize: '5.5rem',
       [theme.breakpoints.down('sm')]: {
         fontSize: '2.5rem'
@@ -42,7 +44,14 @@ const styles = theme => ({
     backgroundColor: red[500],
   },
   cardaction: {
-    margin: theme.spacing()
+    margin: theme.spacing(2)
+  },
+  persons: {
+    marginTop: theme.spacing(5)
+  },
+  description: {
+    whiteSpace: 'pre-line',
+    marginTop: theme.spacing(5)
   }
 });
 
@@ -97,67 +106,80 @@ class ProjectsDetail extends React.Component {
     const name = poster ? poster.full_name ? poster.full_name : poster.username : null
     return (
 
-      <Card className={classes.card}>
-      <Typography variant="h1" component="h1" className={classes.title} gutterBottom>
-        {this.state.title}
-      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={9}>
 
-      {this.state.isPrivate ?
-        <Tooltip title="This project is private" aria-label="add">
-          <Chip variant="outlined" className={classes.chip} color="primary" label="Private" />
-        </Tooltip>
-        : null}
-        <Tooltip title="Deadline for this project" aria-label="add">
-          <Chip variant="outlined" className={classes.chip} color="primary" label={this.state.deadline} icon={<CalendarTodayIcon />}/>
-        </Tooltip>
-      <CardContent>
-      <Typography variant="subtitle1" color="textPrimary" style={{whiteSpace: 'pre-line'}} component="p">
-        {this.state.description}
-      </Typography>
-      </CardContent>
-
-      <Typography color="textSecondary" variant="subtitle2" className={classes.users} component="p">
-        This project is created by
-      </Typography>
-      <CardHeader
-      avatar={
-        <Avatar alt={name} src={poster ? poster.user_profile_pic : null} className={classes.avatar}>
-        {name ? name[0] : ""}
-        </Avatar>
-      }
-      title={name}
-      subheader={this.state.createdAt}
-      />
-
-      <Typography color="textSecondary" variant="subtitle2" className={classes.users} component="p">
-        Developers working on this project
-      </Typography>
-
-      {this.state.developers.length !== 0 ?
-          this.state.developers.map((value, index) => (
-            <CardHeader
-              key={value.id}
-              avatar={
-                <Avatar alt={value.full_name ? value.full_name : value.username} src={value.user_profile_pic} className={classes.avatar}>
-                  {value.full_name ? value.full_name : value.username}
-                </Avatar>
-              }
-              title={value.full_name ? value.full_name : value.username}
-              subheader={value.intro}
-            />
-          ))
-        : (
-          <Typography color="textPrimary" variant="subtitle2" className={classes.users} component="p">
-            Currently no developers are working on this project
+          <Card elevation={0} className={classes.card}>
+          <Typography variant="h1" component="h1" className={classes.title} gutterBottom>
+            {this.state.title}
           </Typography>
-        ) }
 
-      <CardActions className={classes.cardaction}>
-        <Button onClick={() => this.props.history.push(`/p/${this.state.projectId}/progress`)} variant="contained" size="large" color="primary" className={classes.margin}>
-         See Project Progress and Tasks
-       </Button>
-      </CardActions>
-      </Card>
+          {this.state.isPrivate ?
+            <Tooltip title="This project is private" aria-label="add">
+              <Chip variant="outlined" className={classes.chip} color="primary" label="Private" />
+            </Tooltip>
+            : null}
+            <Tooltip title="Deadline for this project" aria-label="add">
+              <Chip variant="outlined" className={classes.chip} color="primary" label={this.state.deadline} icon={<CalendarTodayIcon />}/>
+            </Tooltip>
+          <CardContent>
+          <Typography variant="subtitle1" color="textPrimary" className={classes.description} component="p">
+            {this.state.description}
+          </Typography>
+          </CardContent>
+
+          </Card>
+
+        </Grid>
+
+        <Grid item xs={12} sm={3} className={classes.persons}>
+          <Typography color="textSecondary" variant="subtitle2" className={classes.users} component="p">
+            This project is created by
+          </Typography>
+          <CardHeader
+          avatar={
+            <Avatar alt={name} src={poster ? poster.user_profile_pic : null} className={classes.avatar}>
+            {name ? name[0] : ""}
+            </Avatar>
+          }
+          title={name}
+          subheader={this.state.createdAt}
+          />
+
+          <Typography color="textSecondary" variant="subtitle2" className={classes.users} component="p">
+            Developers working on this project
+          </Typography>
+
+          {this.state.developers.length !== 0 ?
+              this.state.developers.map((value, index) => (
+                <CardHeader
+                  key={value.id}
+                  avatar={
+                    <Avatar alt={value.full_name ? value.full_name : value.username} src={value.user_profile_pic} className={classes.avatar}>
+                      {value.full_name ? value.full_name : value.username}
+                    </Avatar>
+                  }
+                  title={value.full_name ? value.full_name : value.username}
+                  subheader={value.intro}
+                />
+              ))
+            : (
+              <Typography color="textPrimary" variant="subtitle2" className={classes.users} component="p">
+                Currently no developers are working on this project
+              </Typography>
+            ) }
+
+        </Grid>
+
+        <CardActions className={classes.cardaction}>
+          <Button onClick={() => this.props.history.push(`/p/${this.state.projectId}/progress`)} variant="contained" size="large" color="primary" className={classes.margin}>
+           See Project Progress and Tasks
+         </Button>
+        </CardActions>
+
+      </Grid>
+
+
     );
   }
 
