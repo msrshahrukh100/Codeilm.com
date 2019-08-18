e3a9f1d2db9ddf807e4366320c76b227cc2fc69e
Shahrukh <msr.concordfly@gmail.com>
2019-08-18 14:20:03 +0530

    Added static files

--- projects/frontend/projects/copy-build.sh
+++ projects/frontend/projects/copy-build.sh
@@ -1,1 +1,20 @@
 npm run build
+{
+echo "Trying git rm" &&
+git rm -r ../../static/js/ &&
+git rm -r ../../static/css/ &&
+git rm -r ../../static/media/ &&
+echo "Git rm successful" &&
+mkdir ../../static/
+} ||
+{
+echo "Some error occured while doing git rm, removing them using rm"
+rm -r ../../static &&
+echo "rm successful" &&
+mkdir ../../static/
+}
+
+echo "Compying files now"
+cp -r build/static/* ../../static/
+cp -r build/index.html ../../templates/projects.html
+echo "*** Build successfully copied ***"
