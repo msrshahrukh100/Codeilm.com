917019fdf5048881399829e5600d247ff304da47
Mohammad Shahrukh <msr.concordfly@gmail.com>
2019-11-21 15:32:03 +0530

    Started the journey of golang

---
+++ datachamp/src/github.com/msrshahrukh100/datachamp/cmd/root.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "champ",
+	Short: "gets the data from providers",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Usage()
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var configFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(
+		&configFile, "config", "", "config file (default is config.yaml)")
+}
+
+func initConfig() {
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+	} else {
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
+		//viper.AddConfigPath("/etc/bheem")
+		//viper.AddConfigPath("#HOME/.bheem")
+	}
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("unable to read config: %v\n", err)
+		os.Exit(1)
+	}
+}
+
---
+++ datachamp/src/github.com/msrshahrukh100/datachamp/main.go
@@ -0,0 +1,8 @@
+package main
+import (
+    "github.com/msrshahrukh100/datachamp/cmd"
+)
+
+func main() {
+    cmd.Execute()
+}
